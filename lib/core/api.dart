421868461f92128bdc16704e538ab84c50b523c8
nokibul82 <nokibul82@gmail.com>
2024-05-25 22:21:27 +0600

    Assignment-5 ( Third-Party API )

--- lib/core/api.dart
+++ lib/core/api.dart
@@ -1,2 +1,3 @@
 String jsonPlaceholderApi = 'https://jsonplaceholder.typicode.com';
 String jsonPlaceholderApi_id = 'jsonplaceholder.typicode.com';
+String weatherApi = "https://api.openweathermap.org/data/2.5/weather";
