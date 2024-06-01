58b048675cfa1f1465c7f8ad796911ccff1a9103
nokibul82 <nokibul82@gmail.com>
2024-06-01 11:27:22 +0600

    Assignment-5 ( Third-Party API UI )

--- lib/views/screens/weather_screen.dart
+++ lib/views/screens/weather_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:get/get.dart';
 
 import '../../core/app_color.dart';
@@ -55,21 +57,60 @@ class WeatherScreen extends GetView<WeatherController> {
                       onRefresh: () async {
                         await controller.fetchWeather();
                       },
-                      child: SingleChildScrollView(
-                        child: Column(
-                          children: [
-                            SizedBox(
-                              width: 100,
-                              height: 100,
-                              child: Image.network(
-                                "https://openweathermap.org/img/wn/${weatherData.value.weather?[0].icon}@2x.png",
-                                fit: BoxFit.cover,
+                      child: Center(
+                        child: Container(
+                          child: Column(
+                            children: [
+                              Container(
+                                width: 150,
+                                height: 150,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(30),
+                                  color: AppColor.light,
+                                ),
+                                child: Image.network(
+                                  "https://openweathermap.org/img/wn/${weatherData.value.weather?[0].icon}@2x.png",
+                                  fit: BoxFit.cover,
+                                ),
                               ),
-                            ),
-                            Text(weatherData.value.weather?[0].main ?? ""),
-                            Text(weatherData.value.weather?[0].description ??
-                                ""),
-                          ],
+                              Container(
+                                  padding: const EdgeInsets.all(20),
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(30),
+                                      color: AppColor.light),
+                                  child: Text(
+                                    weatherData.value.weather?[0].main ?? "",
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .displayLarge,
+                                  )),
+                              Container(
+                                  padding: const EdgeInsets.all(20),
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(30),
+                                      color: AppColor.light),
+                                  child: Text(
+                                    weatherData.value.weather?[0].description ??
+                                        "",
+                                    style:
+                                        Theme.of(context).textTheme.bodyLarge,
+                                  )),
+                              Container(
+                                  padding: const EdgeInsets.all(20),
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(30),
+                                      color: AppColor.light),
+                                  child: Text(
+                                      "Wind: ${weatherData.value.wind?.speed.toString() ?? ""}")),
+                              Container(
+                                  padding: const EdgeInsets.all(20),
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(30),
+                                      color: AppColor.light),
+                                  child: Text(
+                                      "Location: ${weatherData.value.name.toString() ?? ""}")),
+                            ],
+                          ),
                         ),
                       ));
         }),
